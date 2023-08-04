cfa596f380739a42e3d53b8556e6970932a89840
むずでょ <muzudho1@gmail.com>
2023-08-04 21:17:59 +0900

    画像ファイルピッカー

--- Visual Studio/MauiControlPractice/MauiControlPractice/Views/FilePickerPage.xaml.cs
+++ Visual Studio/MauiControlPractice/MauiControlPractice/Views/FilePickerPage.xaml.cs
@@ -9,4 +9,24 @@ public partial class FilePickerPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    async void LoadFileButton_Clicked(object sender, EventArgs e)
+    {
+        // 非同期で画像ファイル選択
+        var result = await FilePicker.PickAsync(new PickOptions
+        {
+            PickerTitle = "Pick Image Please",
+            FileTypes = FilePickerFileType.Images
+        });
+
+        // ファイルを選んでいなければ終了
+        if (result == null)
+            return;
+
+        // ファイルの読取ストリームを開くところまでしてくれる
+        var stream = await result.OpenReadAsync();
+
+        // 画像コントロールへ、画像データをセット
+        myImage.Source = ImageSource.FromStream(() => stream);
+    }
 }
